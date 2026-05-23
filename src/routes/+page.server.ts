0625a7d32e38c4ab0c8f5fa19f3798f803e92790
Georg <75206704+Georg97@users.noreply.github.com>
2026-05-23 13:44:04 +0200

    updates

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	if (!locals.user) {
-		redirect(302, '/login');
-	}
+	return {
+		isAuthed: !!locals.user,
+		name: locals.user?.name ?? null
+	};
 };
--- src/routes/members/+page.server.ts
+++ src/routes/members/+page.server.ts
@@ -2,7 +2,10 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	if (locals.user) {
-		redirect(302, '/members');
+	if (!locals.user) {
+		redirect(302, '/login');
 	}
+	return {
+		name: locals.user.name ?? null
+	};
 };
